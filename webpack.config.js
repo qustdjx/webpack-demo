72586c87b164efde7c63062d7e6dc6e525b27827
qustdjx <1099629968@qq.com>
2017-03-27 21:44:03 +0800

    添加url-loader
    添加url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: 'style-loader!css-loader'}
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.(png|jpg)$/, loader: "url-loader?limit=8192"}
         ]
     },
     plugins: [
